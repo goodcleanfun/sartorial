204a77f1a3b4a817dcdae86779ce875363129caf
Al <albarrentine@gmail.com>
2024-03-25 03:44:00 -0400

    [test] adding test for Schame with custom types, serialization to JSON Schema and deserialization to a Pydantic model

---
+++ tests/test_schema.py
@@ -0,0 +1,34 @@
+from decimal import Decimal
+
+from corral.schema import Schema
+from corral.serialization import Serializable
+from corral.types import JSONSchemaFormatted
+
+
+def test_custom_type_schema():
+    class CustomTypeA(JSONSchemaFormatted, Serializable):
+        schema_format = "custom-a"
+
+        def __init__(self, value) -> None:
+            if not isinstance(value, str):
+                value = str(value)
+            self.value = value
+
+        def __str__(self) -> str:
+            return self.value
+
+    class Model(Schema):
+        custom: CustomTypeA
+        i: int
+        f: float
+        s: str
+        d: Decimal
+
+    schema = Model.to_schema_dict()
+    NewModel = Schema.from_schema_dict(schema)
+    assert NewModel.__annotations__ == Model.__annotations__
+    n = NewModel(custom="value", i=1, f=1.0, s="string", d="1.0")
+    assert isinstance(n.custom, CustomTypeA)
+    assert n.custom.value == "value"
+    assert isinstance(n.d, Decimal)
+    assert n.d == Decimal("1.0")
