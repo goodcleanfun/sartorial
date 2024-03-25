fd4ac68ad54cb2448a8dbb183a31072886c05ed4
Al <albarrentine@gmail.com>
2024-03-25 02:31:27 -0400

    [test] adding test for custom type serialization

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,7 +1,7 @@
 import json
 from datetime import date, datetime, time, timedelta
 
-from corral.serialization import encode_object
+from corral.serialization import Serializable, decode_object, encode_object
 
 
 def test_encode_object():
@@ -22,3 +22,20 @@ def test_encode_object():
         assert encode_object(o) == e
         j = f'"{e}"' if isinstance(e, str) else str(e)
         assert json.dumps(o, default=encode_object) == j
+
+
+def test_custom_type():
+    class CustomType(Serializable):
+        def __init__(self, value):
+            if not isinstance(value, str):
+                value = str(value)
+            self.value = value
+
+        def __str__(self) -> str:
+            return "custom:" + self.value
+
+    assert encode_object(CustomType("value")) == "custom:value"
+    assert encode_object(CustomType(123)) == "custom:123"
+    decoded = decode_object(CustomType, encode_object(CustomType("value")))
+    assert isinstance(decoded, CustomType)
+    assert decoded.value == "custom:value"
