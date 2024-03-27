f08dbf3f6375770b8efae71e0c38041870789759
Al <albarrentine@gmail.com>
2024-03-27 14:56:43 -0400

    Bump version: 0.4.1 → 0.4.2

--- sartorial/__init__.py
+++ sartorial/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 from .schema import ModelSchema, Schema, StrictModelSchema, StrictSchema
 from .serialization import Serializable, decode_object, encode_object
