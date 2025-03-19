5d3d0a74386dcf28fe74ac7e7a9e7b56077596dd
Al <albarrentine@gmail.com>
2025-03-19 18:02:43 -0400

    Bump version: 0.8.1 → 0.9.0

--- sartorial/__init__.py
+++ sartorial/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 
 from .schema import ModelSchema, Schema, StrictModelSchema, StrictSchema
 from .serialization import Serializable, decode_object, encode_object
